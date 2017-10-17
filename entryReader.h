19f2695d2ef024b52ead7ec280a9611ae592bd8c
Bert54 <matt.bert@live.fr>
2017-10-17 11:19:31 +0200

    Headers moved

--- entryReader.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENTRY_READER
-#define ENTRY_READER
-
-int fileOpener(FILE** file, char* fileName);
-int fileCloser(FILE** file);
-int* fileReader(FILE* file);
-int arrayCleaner(int** arr);
-
-#endif
